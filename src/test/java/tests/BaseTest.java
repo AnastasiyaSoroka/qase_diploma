9f880011506361cee080c800c5ba989c0d7576aa
AnastasiyaSoroka <Lasertag89>
2020-12-17 22:28:57 +0300

    Updated BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,14 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-import pages.LoginPage;
-import pages.ProjectsListPage;
-import steps.LoginSteps;
+import steps.*;
 import utils.CapabilitiesGenerator;
 import utils.PropertyReader;
 import utils.TestListener;
@@ -20,8 +17,10 @@ import utils.TestListener;
 @Listeners(TestListener.class)
 public class BaseTest {
     private WebDriver driver;
-    ProjectsListPage projectsListPage;
+    protected ProjectsSteps projectsSteps;
     protected LoginSteps loginSteps;
+    protected CreateProjectSteps createProjectSteps;
+    protected ProjectSteps projectSteps;
 
     public static final String USERNAME = System.getenv().getOrDefault("username", PropertyReader.getProperty("username"));
     public static final String PASSWORD = System.getenv().getOrDefault("password", PropertyReader.getProperty("password"));
@@ -39,7 +38,9 @@ public class BaseTest {
 
         driver.manage().window().maximize();
         loginSteps = new LoginSteps(driver);
-        projectsListPage = new ProjectsListPage(driver);
+        projectsSteps = new ProjectsSteps(driver);
+        createProjectSteps = new CreateProjectSteps(driver);
+        projectSteps = new ProjectSteps(driver);
 
         context.setAttribute(variable, driver);
 
