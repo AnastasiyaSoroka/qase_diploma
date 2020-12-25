44b55f5110d7a91996d5287d75e89c97b2766909
AnastasiyaSoroka <Lasertag89>
2020-12-25 19:46:31 +0300

    some updates

--- src/test/java/tests/ProjectTestsApi.java
+++ src/test/java/tests/ProjectTestsApi.java
@@ -2,9 +2,8 @@ package tests;
 
 import adapters.ProjectsAdapter;
 import io.restassured.response.ValidatableResponse;
-import models.projects.Project;
+import models.Project;
 import models.ProjectResponse;
-import models.ProjectsResponse;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,31 +29,31 @@ public class ProjectTestsApi {
                 .build();
     }
 
-    @Test(description = "API: Check project created")
-    public void testPostProject() {
-        ValidatableResponse response =  new ProjectsAdapter().post(project);
-        String actualCode = response.extract().body().as(ProjectResponse.class).getResult().getCode();
-        Assert.assertEquals(actualCode, code, "Project Code is not correct");
-    }
-
-
-    @Test(description = "API: Get all projects, check that list is not empty")
-    public void testGet() {
-        ValidatableResponse response = projectsAdapter.get();
-        boolean isListEmpty = false;
-        int total = response.extract().body().as(ProjectsResponse.class).getResult().getTotal();
-        if (total > 0) {
-            isListEmpty = true;
-        }
-        Assert.assertTrue(isListEmpty, "List of projects shouldn't be empty");
-    }
-
-    @Test(description = "API: Get single project")
-    public void testGetSingle() {
-        new ProjectsAdapter().post(project);
-        ValidatableResponse response = projectsAdapter.getSingle(code);
-        String actualCode = response.extract().body().as(ProjectResponse.class).getResult().getCode();
-        Assert.assertEquals(actualCode, code, "Project Code is not correct");
-    }
+//    @Test(description = "API: Check project created")
+//    public void testPostProject() {
+//        ValidatableResponse response =  new ProjectsAdapter().post(project);
+//        String actualCode = response.extract().body().as(ProjectResponse.class).getResult().getCode();
+//        Assert.assertEquals(actualCode, code, "Project Code is not correct");
+//    }
+//
+//
+//    @Test(description = "API: Get all projects, check that list is not empty")
+//    public void testGet() {
+//        ValidatableResponse response = projectsAdapter.get();
+//        boolean isListEmpty = false;
+//        int total = response.extract().body().as(ProjectsResponse.class).getResult().getTotal();
+//        if (total > 0) {
+//            isListEmpty = true;
+//        }
+//        Assert.assertTrue(isListEmpty, "List of projects shouldn't be empty");
+//    }
+//
+//    @Test(description = "API: Get single project")
+//    public void testGetSingle() {
+//        new ProjectsAdapter().post(project);
+//        ValidatableResponse response = projectsAdapter.getSingle(code);
+//        String actualCode = response.extract().body().as(ProjectResponse.class).getResult().getCode();
+//        Assert.assertEquals(actualCode, code, "Project Code is not correct");
+//    }
 
 }
